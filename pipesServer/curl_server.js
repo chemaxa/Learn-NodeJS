8d9b1e0d7997e63ecd01d5bac8554c51236f9c88
chemaxa@gmail.com <chemaxa@gmail.com>
2015-10-20 17:45:37 +0300

    Add curl server

---
+++ pipesServer/curl_server.js
@@ -0,0 +1,28 @@
+'use strict';
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function(req, res) {
+
+    var newFile = fs.createWriteStream('destination.jpg');
+    var fileBytes = req.headers['content-length'];
+    var uploadedBytes = 0;
+
+    req.on('readable', function() {
+        var chunk = null;
+        while (null !== (chunk = req.read())) {
+            uploadedBytes += chunk.length;
+            var progress = (uploadedBytes / fileBytes) * 100;
+            res.write("Progress: " + parseInt(progress, 10) + "%\n");
+        }
+    });
+
+    req.on('end', function() {
+        res.end('uploaded!');
+    });
+
+    req.pipe(newFile);
+
+}).listen(8080);
+
+// run with `curl --upload-file <your-file.jpg> http://localhost:8080`
--- pipesServer/server.js
+++ pipesServer/server.js
@@ -1,67 +1,69 @@
+// Working with localhost:3000
 'use strict';
 let http = require('http'),
     fs = require('fs'),
-    //enctype = 'application/x-www-form-urlencoded',
-    enctype = 'multipart/form-data',
-    //descr = '<input type = "text" name = "description">',
+    enctype = 'multipart/form-data', // enctype
+    //descr = '<input type = "text" name = "description">', // Description for uploaded file, not work :(
     descr = '',
     form =
-    '<form id = "fileForm" enctype="' + enctype + '"  method = "POST">' + '<input type = "file" name = "file">' + descr + '<button> Send file </button> </form>';
-const ROOT = __dirname + '\\';
+    '<form id = "fileForm" enctype="' + enctype + '"  method = "POST">' + '<input type = "file" name = "file">' + descr + '<button> Send file </button> </form>'; // HTML form
+const ROOT = __dirname + '\\'; // Upload in current dir
 
 http.createServer((req, res) => {
-    if (req.method == 'POST') {
-        let body = '';
+    if (req.method == 'POST') { // If POST
+        let body = ''; // Body of POST request
         req.on('data', (chunk) => {
-            body += chunk;
+            body += chunk; // get bytes 
         });
 
         req.on('end', () => {
-            parsePost(req, body);
+            parsePost(req, body); // Parse POST
         });
     }
+    // Set Headers
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    res.write(form);
-    res.end('The end');
+    res.write(form); // Send form to client
+    res.end('The end'); // close connection
 }).listen(3000);
 
 function parsePost(req, body) {
-    let boundary,
-        content,
-        disposition,
-        type,
-        filename,
-        pos = 0;
+    let boundary, // boundary for multipart
+        content, // binary content from POST
+        disposition, // Content-Disposition from headers
+        type, // Content-Type from headers
+        filename, // filename as filename.png
+        pos = 0; // start pos for parsing
 
-    for (let i = 4; i; i--) {
+    for (let i = 4; i; i--) { // parse POST as string
         var foundPos = body.indexOf('\r\n', pos);
         switch (i) {
-            case 4:
+            case 4: // Get boundary
                 boundary = body.slice(pos + 1, foundPos);
                 break;
-            case 3:
+            case 3: // Disposition
                 disposition = body.slice(pos + 1, foundPos);
                 break;
-            case 2:
+            case 2: // Type
                 type = body.slice(pos + 1, foundPos);
                 break;
         }
         pos = foundPos + 1;
     }
-
+    // Get filename
     filename = parseFilename(disposition);
 
     content = body.slice(foundPos, body.indexOf(boundary, foundPos));
+    // Debug :)
     //console.log('BD:', boundary, boundary.length);
     //console.log('DP:', disposition, disposition.length);
     //console.log('TP:', type, type.length);
     //console.log('CT:', content, content.length);
     //console.log('Body:', body, body.length);
-
+    // Write files< not work :(
     fs.writeFile(ROOT + filename, content, 'binary', function(err) {
         if (err) throw err;
-        console.error('Wrote OK');
+        console.log('Wrote OK');
     });
 
 }
