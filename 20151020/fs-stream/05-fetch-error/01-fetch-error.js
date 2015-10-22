1ae5f354d5be0ce9bbe0eeddce74ff2fb40d3193
Maxim <chemaxa@gmail.com>
2015-10-22 20:23:26 +0300

    Handling errors

--- 20151020/fs-stream/05-fetch-error/01-fetch-error.js
+++ 20151020/fs-stream/05-fetch-error/01-fetch-error.js
@@ -16,27 +16,27 @@ function fetch(srcUrl, destPath, callback) {
     // request - поток, которые делает запрос
     var request = http.get(requestOptions);
 
+    request.on('error', callback);
 
     // response - поток, из которого мы читаем ответ
     request.on('response', function(response) {
-
-        response.on('error', function(err) {
-            console.log(err);
-        });
-
+        response.on('error', callback);
         if (response.headers['content-encoding']) {
             response = response
                 .pipe(zlib.createUnzip());
         }
-
-        response
-            .pipe(fs.createWriteStream(destPath))
-            .on('finish', callback);
+        let file = fs.createWriteStream(destPath, {
+            flags: 'wx+'
+        });
+        file.on('error', callback);
+        response.pipe(file);
+        response.on('error', callback);
+        response.on('finish', callback);
     });
 }
 
 
-fetch('http://162ya.ru', './result.html', function(err) {
+fetch('http://ya.ru', './result.html', function(err) {
     if (err) console.error(err);
     else console.log('OK!');
 });
