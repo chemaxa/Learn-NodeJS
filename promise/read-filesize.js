6595caae5f00e78028684af474001980ab1bbe24
chemaxa@gmail.com <chemaxa@gmail.com>
2015-10-28 11:21:02 +0300

    Promises working...

--- promise/read-filesize.js
+++ promise/read-filesize.js
@@ -4,10 +4,10 @@
 'use strict';
 let fs = require('mz/fs'),
     util = require('util'),
-    summ = 0,
-    file = 'text.txt';
+    summ = 0;
 
-function summFileSizes(path) {
+
+function getFile(path) {
     return new Promise(function(resolve, reject) {
         fs.stat(path, (err, data) => {
             if (err) return reject(err);
@@ -16,8 +16,40 @@ function summFileSizes(path) {
     });
 }
 
-summFileSizes(file).then(function(data) {
-    if (data.isFile())
-        summ += util.inspect(data).size;
-    console.log(data);
+function readDir(path) {
+    return new Promise((resolve, reject) => {
+        fs.stat(path, (err, data) => {
+            if (err) return reject(err);
+            if (data.isDirectory()) {
+                fs.readdir(path, (err, data) => {
+                    if (err) return reject(err);
+                    resolve(data);
+                });
+            }
+        });
+    });
+}
+
+function promisification(arr) {
+    let tmp = arr.map((item) => {
+        return getFile(item).then((data) => {
+            if (data.isFile())
+                summ += data.size;
+        }).catch(console.log);
+    });
+    console.log('TMP', tmp);
+    return tmp;
+}
+
+readDir('../promise').then((pathArr) => {
+    Promise.all(promisification(pathArr)).then((sum) => {
+        console.log('Summ of files: ', sum);
+    }).catch(console.log);
 }).catch(console.log);
+
+/*summFileSizes(__filename).then(function(data) {
+    if (data.isFile())
+        summ += data.size;
+    console.log(summ);
+}).catch(console.log);
+*/
